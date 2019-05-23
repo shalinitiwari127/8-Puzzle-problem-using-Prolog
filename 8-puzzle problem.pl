f8b45481bc6f1aeadaa58543a039e7b8207e4a22
Shalini Tiwari <47652026+shalinitiwari127@users.noreply.github.com>
2019-05-22 21:09:47 -0700

    Add files via upload

---
+++ 8-puzzle problem.pl
@@ -0,0 +1,78 @@
+goal([1,2,3,
+      4,0,5,
+      6,7,8]).
+
+move([X1,0,X3, X4,X5,X6, X7,X8,X9],
+     [0,X1,X3, X4,X5,X6, X7,X8,X9]).
+move([X1,X2,0, X4,X5,X6, X7,X8,X9],
+     [X1,0,X2, X4,X5,X6, X7,X8,X9]).
+
+
+move([X1,X2,X3, X4,0,X6,X7,X8,X9],
+     [X1,X2,X3, 0,X4,X6,X7,X8,X9]).
+move([X1,X2,X3, X4,X5,0,X7,X8,X9],
+     [X1,X2,X3, X4,0,X5,X7,X8,X9]).
+
+move([X1,X2,X3, X4,X5,X6, X7,0,X9],
+     [X1,X2,X3, X4,X5,X6, 0,X7,X9]).
+move([X1,X2,X3, X4,X5,X6, X7,X8,0],
+     [X1,X2,X3, X4,X5,X6, X7,0,X8]).
+
+move([0,X2,X3, X4,X5,X6, X7,X8,X9],
+     [X2,0,X3, X4,X5,X6, X7,X8,X9]).
+move([X1,0,X3, X4,X5,X6, X7,X8,X9],
+     [X1,X3,0, X4,X5,X6, X7,X8,X9]).
+
+move([X1,X2,X3, 0,X5,X6, X7,X8,X9],
+     [X1,X2,X3, X5,0,X6, X7,X8,X9]).
+move([X1,X2,X3, X4,0,X6, X7,X8,X9],
+     [X1,X2,X3, X4,X6,0, X7,X8,X9]).
+
+
+move([X1,X2,X3, X4,X5,X6,0,X8,X9],
+     [X1,X2,X3, X4,X5,X6,X8,0,X9]).
+move([X1,X2,X3, X4,X5,X6,X7,0,X9],
+     [X1,X2,X3, X4,X5,X6,X7,X9,0]).
+
+
+move([X1,X2,X3, 0,X5,X6, X7,X8,X9],
+     [0,X2,X3, X1,X5,X6, X7,X8,X9]).
+move([X1,X2,X3, X4,0,X6, X7,X8,X9],
+     [X1,0,X3, X4,X2,X6, X7,X8,X9]).
+move([X1,X2,X3, X4,X5,0, X7,X8,X9],
+     [X1,X2,0, X4,X5,X3, X7,X8,X9]).
+
+
+move([X1,X2,X3, X4,X5,X6, X7,0,X9],
+     [X1,X2,X3, X4,0,X6, X7,X5,X9]).
+move([X1,X2,X3, X4,X5,X6, X7,X8,0],
+     [X1,X2,X3, X4,X5,0, X7,X8,X6]).
+move([X1,X2,X3, X4,X5,X6, 0,X8,X9],
+     [X1,X2,X3, 0,X5,X6, X4,X8,X9]).
+
+
+move([0,X2,X3, X4,X5,X6, X7,X8,X9],
+     [X4,X2,X3, 0,X5,X6, X7,X8,X9]).
+move([X1,0,X3, X4,X5,X6, X7,X8,X9],
+     [X1,X5,X3, X4,0,X6, X7,X8,X9]).
+move([X1,X2,0, X4,X5,X6, X7,X8,X9],
+     [X1,X2,X6, X4,X5,0, X7,X8,X9]).
+
+
+move([X1,X2,X3, 0,X5,X6, X7,X8,X9],
+     [X1,X2,X3, X7,X5,X6, 0,X8,X9]).
+move([X1,X2,X3, X4,0,X6, X7,X8,X9],
+     [X1,X2,X3, X4,X8,X6, X7,0,X9]).
+move([X1,X2,X3, X4,X5,0, X7,X8,X9],
+     [X1,X2,X3, X4,X5,X9, X7,X8,0]).
+
+dfsSimplest(S, [S]) :- goal(S).
+dfsSimplest(S, [S|Rest]) :- move(S, S2), dfsSimplest(S2, Rest).
+%%dfsSimplest([1,2,3, 0,4,5, 6,7,8], Path).
+
+dfs(S, Path, Path) :- goal(S).
+dfs(S, Checked, Path) :-    move(S, S2),
+        \+member(S2, Checked),
+    dfs(S2, [S2|Checked], Path).
+
+%% dfs([1,2,3, 0,4,5, 6,7,8], [], Path).
